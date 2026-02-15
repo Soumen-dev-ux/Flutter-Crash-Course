8f423db3a61e1b789333399cd3b5ac291c2f215e
Soumen-dev-ux <soumenpore2005@gmail.com>
2026-02-15 12:22:28 +0530

    Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,115 @@
+
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
+// stateless section
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      home: CounterScreen(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.cyan
-      ),
-      home: home(),
     );
   }
 }
 
-class home extends StatelessWidget {
-  const home();
+//stattefull section
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int counter = 0;
+  void _incrementCounter(){
+    setState(() {
+      counter++;
+    });
+  }
+  void _decrementCounter(){
+    setState(() {
+      counter--;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Soumen Pore', style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-          fontSize: 25,
+        title: Text('Counter App',
+        style: TextStyle(
+          fontWeight: FontWeight.bold
         ),
         ),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
-      body:
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue.shade200, Colors.blueAccent.shade700],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-
-            )),
-          child: Padding(
-              padding: const EdgeInsets.all(20.0),
-          child:
-            Card(
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Your Current Count',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.cyan,
               ),
-              shadowColor: Colors.blueAccent.withOpacity(0.3),
-              child: Padding(
-                  padding: const EdgeInsets.all(24),
-                child: Column(
-                  children: [
-                    Text('Welcome to my App',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 20,),
-                    Image(image: AssetImage('assets/AICTE.png',),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton(
-                            onPressed: (){},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)
-                              ),
-                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10)
-                            ),
-                            child: Text('Subscribe', style: TextStyle(color: Colors.white),)),
-                        ElevatedButton(
-                            onPressed: (){},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blueAccent,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)
-                              ),
-                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10)
-                            ),
-                            child: Text('➕ Follow', style: TextStyle(color: Colors.white),))
-                      ],
-                    )
-                  ]
-                )
-              )
-          )
-          )
-        )
+            ),
+            SizedBox(height: 10),
+            Text(
+              '${counter}',
+              style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurpleAccent,
+              ),
+            ),
+            SizedBox(height:30,),
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: _decrementCounter,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple[100],
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15 ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                    child: Icon(Icons.remove, size: 30, color: Colors.deepPurple,)
+                ),
+                SizedBox(width: 30,),
 
-
+                ElevatedButton(
+                    onPressed: _incrementCounter,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15 ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    child: Icon(Icons.add, size: 30, color: Colors.deepPurple[100],)
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
     );
   }
 }
